515cfb631fcf3d6349047e4eba55ce35276e1dbc
chloe11c <162982857+chloe11c@users.noreply.github.com>
2024-03-11 12:30:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write ("Hello"+name)
 genre = st.radio(
     "What's your favorite movie genre",
     [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
-    captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+    captions = ["Laugh out loud.", "HIHIIHIH AHHAHAH.", "Never stop learning."])
 
 if genre == ':rainbow[Comedy]':
     st.write('You selected comedy.')
