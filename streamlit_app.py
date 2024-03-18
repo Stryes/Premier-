b16ef3646febd0d0ec727ba238ba0cddc61c2f71
chloe11c <162982857+chloe11c@users.noreply.github.com>
2024-03-18 12:12:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q5-d-BiQXk6JD4r--bTZ3lNcryvfCeKqJq-tnz3VLhPwS_bZ5WXsRGYkOrtgkLlTU/pub?output=csv")
 l=voc.shape[0]
 i=np.random.choice(range(l))
-world_fr=voc["Définition"].values[i]
-world_chi=voc["Pinyin"].values[i]
+world_fr=voc['Définition'].values[i]
+world_chi=voc['Pinyin'].values[i]
 st.write(word_fr+" "+word_chi)
 st.button("refresh")
 
