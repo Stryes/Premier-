1c8afaa0b62839450e894d4b20f09ec94553d200
chloe11c <162982857+chloe11c@users.noreply.github.com>
2024-03-18 11:55:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ for i in range (len(list_possibilities)):
 
 import streamlit as st 
 import pandas as pd 
-voc=pd.read_csv("https://docs.google.com/spreadsheets/d/1jH6kJJISFA0Ye4gE4CzG7ZEjBJhCRelGOZN-HnxyZBQ/edit#gid=0")
+voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q5-d-BiQXk6JD4r--bTZ3lNcryvfCeKqJq-tnz3VLhPwS_bZ5WXsRGYkOrtgkLlTU/pub?output=csv")
 st.dataframe(voc)
 
 
