8d2ab7878cc726706794de241d99d2d510ed1955
chloe11c <162982857+chloe11c@users.noreply.github.com>
2024-03-18 12:13:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 import pandas as pd 
+import numpy as np 
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q5-d-BiQXk6JD4r--bTZ3lNcryvfCeKqJq-tnz3VLhPwS_bZ5WXsRGYkOrtgkLlTU/pub?output=csv")
 l=voc.shape[0]
 i=np.random.choice(range(l))
