65f7983c0044a62b004c4f6bd33d449d73e57518
chloe11c <162982857+chloe11c@users.noreply.github.com>
2024-03-18 12:03:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ import streamlit as st
 import pandas as pd 
 voc=pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTaUESVFbUnxC9q5-d-BiQXk6JD4r--bTZ3lNcryvfCeKqJq-tnz3VLhPwS_bZ5WXsRGYkOrtgkLlTU/pub?output=csv")
 st.dataframe(voc)
-l=voc.shape(voc)
+l=voc.shape[0]
 i=hp.random.choice(range(l))
 world_fr=voc["Definition"].values[i]
 world_chi=voc["Pinyin"].values[i]
